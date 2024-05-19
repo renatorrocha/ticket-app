f680200106c8559dd00a62404dda74350dcc8543
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-19 16:11:15 -0300

    styling navgation links

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,16 +1,13 @@
 import Link from "next/link";
 import React from "react";
 import ToggleTheme from "./toggle-theme";
+import MainNavLinks from "./main-nav-links";
 
 export default function MainNav() {
   return (
     <div className="flex justify-between">
       <div className="flex items-center gap-2">
-        <Link href="/">Dashboard</Link>
-
-        <Link href="/tickets">Tickets</Link>
-
-        <Link href="/users">Users</Link>
+        <MainNavLinks />
       </div>
 
       <div className="flex items-center gap-2">
