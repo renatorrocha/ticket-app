7a35a03f9a1cf6d252bd14d804341942bb80dd87
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-19 16:03:37 -0300

    feat dark mode with theme switch

---
+++ components/toggle-theme.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export default function ToggleTheme() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Button variant="outline" size="icon" disabled></Button>;
+  }
+
+  const dark = theme === "dark";
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(dark ? "light" : "dark")}
+    >
+      {dark ? (
+        <Sun className="hover:cursor-pointer hover:text-primary" />
+      ) : (
+        <Moon className="hover:cursor-pointer hover:text-primary" />
+      )}
+    </Button>
+  );
+}
