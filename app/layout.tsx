bded28ee79069f5da1a431459c0d9ee6bd280e72
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-19 15:36:31 -0300

    styling layout and nav-bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <MainNav />
-        {children}
+        <nav className="flex flex-col items-center border-b mb-5 px-5 py-3">
+          <div className="max-w-6xl w-full">
+            <MainNav />
+          </div>
+        </nav>
+
+        <main className="flex flex-col items-center">
+          <div className="max-w-6xl w-full">{children}</div>
+        </main>
       </body>
     </html>
   );
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,12 +3,20 @@ import React from "react";
 
 export default function MainNav() {
   return (
-    <div>
-      <Link href="/">Dashboard</Link>
+    <div className="flex justify-between">
+      <div className="flex items-center gap-2">
+        <Link href="/">Dashboard</Link>
 
-      <Link href="/tickets">Tickets</Link>
+        <Link href="/tickets">Tickets</Link>
 
-      <Link href="/users">Users</Link>
+        <Link href="/users">Users</Link>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <Link href="/users">Logout</Link>
+
+        <Link href="/users">Dark</Link>
+      </div>
     </div>
   );
 }
