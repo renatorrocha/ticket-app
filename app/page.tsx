3f6156fecb8747792c1d9a10feb1c2725a4b49db
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-27 16:16:19 -0300

    feat(dashboard):  tickets chart

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,13 @@ export default async function Dashboard() {
     },
   });
 
+  const data = groupTicket.map((item) => {
+    return {
+      name: item.status,
+      total: item._count.id,
+    };
+  });
+
   return (
     <div>
       <div className="grid gap-4 md:grid-cols-2 px-2">
@@ -32,7 +39,7 @@ export default async function Dashboard() {
         </div>
 
         <div className="">
-          <DashChart />
+          <DashChart data={data} />
         </div>
       </div>
     </div>
--- components/dash-chart.tsx
+++ components/dash-chart.tsx
@@ -1,5 +1,51 @@
-import React from "react";
+"use client";
 
-export default function DashChart() {
-  return <div>DashChart</div>;
+import { Status } from "@prisma/client";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Bar, BarChart, ResponsiveContainer, XAxis } from "recharts";
+
+interface dataProps {
+  data: dataElements[];
+}
+
+interface dataElements {
+  name: Status;
+  total: number;
+}
+
+export default function DashChart({ data }: dataProps) {
+  return (
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle>Ticket Counts</CardTitle>
+      </CardHeader>
+
+      <CardContent className="pl-2">
+        <ResponsiveContainer width="100%" height={350}>
+          <BarChart data={data}>
+            <XAxis
+              dataKey="name"
+              stroke="hsl(var(--secondary-foreground))"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+
+            <XAxis
+              stroke="hsl(var(--secondary-foreground))"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+
+            <Bar
+              dataKey="total"
+              fill="hsl(var(--primary))"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  );
 }
