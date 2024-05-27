0f27cb1483b81b2b0c57e8e328148de104f1f3c9
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-05-26 22:28:48 -0300

    feat(users): user Table

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,15 @@
 import UserForm from "@/components/user-form";
-import React from "react";
+import DataTableSimple from "./data-table";
+import prisma from "@/prisma/db";
 
-export default function UsersPage() {
+export default async function UsersPage() {
+  const users = await prisma?.user.findMany();
+
   return (
     <div>
       <UserForm />
+
+      <DataTableSimple users={users} />
     </div>
   );
 }
